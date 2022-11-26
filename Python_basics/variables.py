e5fe7176b2022d701bdbe88be0c21be68367814a
AlkeshSakhare <akleshsakhare@yahoo.com>
2022-11-26 15:20:17 +0530

    update

--- Python_basics/variables.py
+++ Python_basics/variables.py
@@ -1,6 +1,19 @@
-a=input("Enter no 1: ")
-b=input("Enter no 2: ")
+# integer
+a=100
+b=50
+c=a+b
+print("a,b : ", a,b)
+print("Add: ",c)
+c=a-b
+print("Sub: ",c)
+c=a*b
+print("Mul: ",c)
+c=a/b
+print("Div: ",c)
 
-print(a+b) # default data type is string
-print(int(a)+int(b)) # converting string to int
+# boolean
+c=a==b
+print("Compare a==b : ",c)
 
+c=a!=b
+print("Compare a!=b: ",c)

9fb99cb8ff02363091f2fd2a9820cd0d107a4a88
AlkeshSakhare <akleshsakhare@yahoo.com>
2022-12-01 20:46:43 +0530

    updated

---
+++ Python_basics/collections/list_demo.py
@@ -0,0 +1,46 @@
+# Creating list
+mobile_brand = ["Mi", 'Redmi', 'Samsung', 'Oppp',
+                'Vivo', 'Google', 'Motorola', 'OnePlus', 'iPhone']
+print(type(mobile_brand))
+print(mobile_brand)
+
+# read items from list
+mobile_brand = ["Mi", 'Redmi', 'Samsung', 'Oppp',
+                'Vivo', 'Google', 'Motorola', 'OnePlus', 'iPhone']
+print("Reading by index 1: ", mobile_brand[1])
+
+# reading by for loop
+mobile_brand = ["Mi", 'Redmi', 'Samsung', 'Oppp',
+                'Vivo', 'Google', 'Motorola', 'OnePlus', 'iPhone']
+for i in mobile_brand:
+    print(i, "\t", end="")
+
+# adding element
+mobile_brand = ["Mi", 'Redmi', 'Samsung', 'Oppp',
+                'Vivo', 'Google', 'Motorola', 'OnePlus', 'iPhone']
+mobile_brand.append("Jio")
+print(mobile_brand)
+
+# updaing element
+mobile_brand = ["Mi", 'Redmi', 'Samsung', 'Oppp',
+                'Vivo', 'Google', 'Motorola', 'OnePlus', 'iPhone']
+mobile_brand[0] = mobile_brand[1]
+print(mobile_brand)
+
+# removing element
+mobile_brand = ["Mi", 'Redmi', 'Samsung', 'Oppp',
+                'Vivo', 'Google', 'Motorola', 'OnePlus', 'iPhone']
+mobile_brand.remove("OnePlus")
+print(mobile_brand)
+
+# Size of list
+mobile_brand = ["Mi", 'Redmi', 'Samsung', 'Oppp',
+                'Vivo', 'Google', 'Motorola', 'OnePlus', 'iPhone']
+print("No of elements in mobile_brand list: ", len(mobile_brand))
+
+# delete list
+mobile_brand = ["Mi", 'Redmi', 'Samsung', 'Oppp',
+                'Vivo', 'Google', 'Motorola', 'OnePlus', 'iPhone']
+print("Before delete: ", mobile_brand)
+del mobile_brand
+print("After delete: ", mobile_brand)

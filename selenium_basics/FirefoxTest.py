b958169ae36605a89eb6d92c3522c29bfed1d636
AlkeshSakhare <akleshsakhare@yahoo.com>
2022-12-04 08:23:50 +0530

    preventing geckodriver.log file

--- selenium_basics/FirefoxTest.py
+++ selenium_basics/FirefoxTest.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service as FirefoxService
 
-url="http://the-internet.herokuapp.com/login"
-driver=webdriver.Firefox()
+url = "http://the-internet.herokuapp.com/login"
+# prevent creating geckodriver.log file
+service = FirefoxService(log_path='nul')
+driver = webdriver.Firefox(service=service)
 driver.maximize_window()
 driver.implicitly_wait(20)
 driver.get(url)
-driver.find_element(By.XPATH,"//input[@id='username']").send_keys("username")
-driver.find_element(By.XPATH,"//input[@id='password']").send_keys("password")
+driver.find_element(By.XPATH, "//input[@id='username']").send_keys("username")
+driver.find_element(By.XPATH, "//input[@id='password']").send_keys("password")
 driver.save_screenshot("./screenshot/image.png")
-
-
+driver.quit()
